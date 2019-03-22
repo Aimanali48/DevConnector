9d400f3ece5d0aeff2f20061093fa94ca9986baf
Aimanali48 <aimanaly48@gmail.com>
2019-03-22 20:53:54 +0500

    apply middleware error resolved

--- client/src/store.js
+++ client/src/store.js
@@ -3,9 +3,8 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const initialState = {}
-const middleware = [thunk]
 
 const store = createStore(rootReducer,initialState, 
-    compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+    compose(applyMiddleware(thunk)))
 
 export default store;
